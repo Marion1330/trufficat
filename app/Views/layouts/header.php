b76d1f79d23ad37dbf5bbd9f3bc0ce7f48653e94
Marion1330 <webberMarion@hotmail.com>
2025-05-06 13:59:22 +0200

    menu déroulant compte connexion

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -8,24 +8,24 @@
 <body>
 <header>
 <nav class="navbar">
-<div class="logo">
-<a href="/"><img src="/trufficat/public/images/logo.png" alt="Trufficat" class="logo-img"></a>
-</div>
-<form action="/recherche" method="get" class="search-bar">
-<input type="text" name="q" placeholder="Rechercher un produit...">
-</form>
+    <div class="logo">
+        <a href="/"><img src="/trufficat/public/images/logo.png" alt="Trufficat" class="logo-img"></a>
+    </div>
+    <form action="/recherche" method="get" class="search-bar">
+        <input type="text" name="q" placeholder="Rechercher un produit...">
+    </form>
 
-<ul class="nav-links">
-<li><a href="/">Accueil</a></li>
-<li class="dropdown">
-<a href="#">Compte</a>
-<ul class="dropdown-menu">
-<li><a href="/login">Se connecter</a></li>
-<li><a href="/register">S'inscrire</a></li>
-</ul>
-</li>
-<li><a href="/panier">Panier</a></li>
-</ul>
+    <ul class="nav-links">
+        <li><a href="/">Accueil</a></li>
+        <li class="dropdown">
+            <a href="#">Compte</a>
+            <ul class="dropdown-menu">
+                <li><a href="/login">Connexion</a></li>
+                <li><a href="/register">S'inscrire</a></li>
+            </ul>
+        </li>
+        <li><a href="/panier">Panier</a></li>
+    </ul>
 </nav>
 </header>
 
